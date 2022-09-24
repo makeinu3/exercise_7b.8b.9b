eaeab316ffe4f43e375d3f3a23b106421ef3c0b8
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-09-24 03:03:31 +0000

    [Add] コメント機能実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,8 +1,24 @@
 class BookCommentsController < ApplicationController
 
   def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comments_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to book_path(book)
   end
 
   def destroy
+    book = Book.find(params[:book_id])
+    BookComment.find(params[:id]).destroy
+    redirect_to book_path(book)
   end
+
+
+  private
+
+  def book_comments_params
+    params.require(:book_comment).permit(:comment)
+  end
+
 end
