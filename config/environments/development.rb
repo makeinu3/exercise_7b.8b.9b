98049beb40a37aae631bcef6cefce9238d65ad09
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-10-11 03:17:54 +0000

    [Finish] 応用課題9c　メール送信機能実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,16 +76,9 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts.clear
 
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port:                 587,
-    address:              'smtp.gmail.com',
-    domain:               'gmail.com',
-    user_name:            '<YOUR EMAIL ADDRESS>',
-    password:             '<YOUR EMAIL PASSWORD>',
-    authentication:       'login',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
 end
